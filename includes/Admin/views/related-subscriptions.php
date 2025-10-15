a834be507a91c4137fe1d1030b001d22c959c9ef
aushamim <au.shamim0@gmail.com>
2025-10-15 17:36:23 +0600

    fix: 🐛 added subscription info on related subscriptions of order

--- includes/Admin/views/related-subscriptions.php
+++ includes/Admin/views/related-subscriptions.php
@@ -8,8 +8,13 @@
 use SpringDevs\Subscription\Illuminate\Helper;
 
 ?>
+<style>
+	.subscrpt-related-subscriptions td{
+		vertical-align: middle;
+	}
+</style>
 
-<table class="widefat striped">
+<table class="widefat striped subscrpt-related-subscriptions">
 	<thead>
 		<tr>
 			<th><?php esc_html_e( 'ID', 'wp_subscription' ); ?></th>
@@ -23,15 +28,24 @@ use SpringDevs\Subscription\Illuminate\Helper;
 	<tbody>
 		<?php
 		foreach ( $histories as $history ) :
-			$subscription_id = $history->subscription_id;
-			$order_item_id   = get_post_meta( $history->subscription_id, '_subscrpt_order_item_id', true );
-			$order_item      = $order->get_item( $history->order_item_id );
-			$trial           = get_post_meta( $history->subscription_id, '_subscrpt_trial', true );
-			$start_date      = get_post_meta( $history->subscription_id, '_subscrpt_start_date', true );
-			$next_date       = get_post_meta( $history->subscription_id, '_subscrpt_next_date', true );
-			$status_object   = get_post_status_object( get_post_status( $history->subscription_id ) );
+			$subscription_id   = $history->subscription_id;
+			$subscription_data = Helper::get_subscription_data( $subscription_id );
 
-			$price          = get_post_meta( $history->subscription_id, '_subscrpt_price', true );
+			$subscrpt_status = $subscription_data['status'] ?? '';
+			$verbose_status  = Helper::get_verbose_status( $subscrpt_status );
+
+			$order_item_id = get_post_meta( $history->subscription_id, '_subscrpt_order_item_id', true );
+			$order_item    = $order->get_item( $history->order_item_id );
+
+			$trial = get_post_meta( $history->subscription_id, '_subscrpt_trial', true );
+
+			$start_date = $subscription_data['start_date'] ?? '';
+			$start_date = ! empty( $start_date ) ? gmdate( 'F j, Y - g:i A', strtotime( $start_date ) ) : __( 'N/A', 'wp_subscription' );
+
+			$next_date = $subscription_data['next_date'] ?? '';
+			$next_date = ! empty( $next_date ) ? gmdate( 'F j, Y - g:i A', strtotime( $next_date ) ) : __( 'N/A', 'wp_subscription' );
+
+			$price          = $subscription_data['price'] ?? 0;
 			$price_excl_tax = (float) $order_item->get_total();
 			$tax_amount     = (float) $order_item->get_total_tax();
 
@@ -39,6 +53,9 @@ use SpringDevs\Subscription\Illuminate\Helper;
 				$price = $price_excl_tax + $tax_amount;
 				$price = number_format( (float) $price, 2, '.', '' );
 			}
+
+			$is_grace_period = isset( $subscription_data['grace_period'] );
+			$grace_remaining = $subscription_data['grace_period']['remaining_days'] ?? 0;
 			?>
 				<tr>
 					<td>
@@ -47,12 +64,39 @@ use SpringDevs\Subscription\Illuminate\Helper;
 					<td>
 						<?php echo esc_html( $order_item->get_name() ); ?>
 					</td>
-					<td><?php echo wp_kses_post( Helper::format_price_with_order_item( $price, $order_item->get_id() ) ); ?></td>
 					<td>
-						<?php echo null == $trial ? ( ! empty( $start_date ) ? esc_html( gmdate( 'F d, Y', $start_date ) ) : '-' ) : '+' . esc_html( $trial ) . ' ' . esc_html__( 'free trial', 'wp_subscription' ); ?>
+						<?php echo wp_kses_post( Helper::format_price_with_order_item( $price, $order_item->get_id() ) ); ?>
 					</td>
-					<td><?php echo esc_html( ! empty( $start_date ) && ! empty( $next_date ) ? ( $trial == null ? gmdate( 'F d, Y', $next_date ) : gmdate( 'F d, Y', $start_date ) ) : '-' ); ?></td>
-					<td><span class="subscrpt-<?php echo esc_attr( $status_object->name ); ?>"><?php echo esc_html( $status_object->label ); ?></span></td>
+					<td>
+						<?php echo esc_html( $start_date ); ?>
+					</td>
+					<td>
+						<?php echo esc_html( $next_date ); ?>
+					</td>
+					<!-- <td><span class="subscrpt-<?php echo esc_attr( $status_object->name ); ?>"><?php echo esc_html( $status_object->label ); ?></span></td> -->
+
+					<td>
+						<?php if ( $is_grace_period && $grace_remaining > 0 ) : ?>
+							<span class="subscrpt-active grace-active">
+								Active
+
+								<?php
+									$grace_remaining_text = sprintf(
+										// translators: Number of days remaining in grace period.
+										__( '%d days remaining!', 'wp_subscription' ),
+										$grace_remaining
+									);
+								?>
+								<span class="grace-icon" data-tooltip="<?php echo esc_attr( $grace_remaining_text ); ?>">
+									<span class="dashicons dashicons-warning"></span>
+								</span>
+							</span>
+						<?php else : ?>
+							<span class="subscrpt-<?php echo esc_attr( strtolower( $subscrpt_status ) ); ?>">
+								<?php echo esc_html( $verbose_status ); ?>
+							</span>
+						<?php endif; ?>
+					</td>
 				</tr>
 				<?php
 		endforeach;
