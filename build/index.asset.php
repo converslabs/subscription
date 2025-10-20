6dd0323fc794ba6829366ddbf4ace0e64a2079f2
aushamim <au.shamim0@gmail.com>
2025-10-20 11:45:13 +0600

    release: 🎉 v1.9.0

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-blocks-components', 'wc-price-format', 'wp-i18n', 'wp-plugins'), 'version' => '4d073083dc9c4e269dca');
+<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-blocks-components', 'wc-price-format', 'wp-i18n', 'wp-plugins'), 'version' => 'c14d0a799ab35b74a540');
