fca1e1f387daaec99c6566e7244a9ece1a1f57b9
aushamim <au.shamim0@gmail.com>
2025-11-18 17:45:07 +0600

    feat: ✨ added flowbite

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-blocks-components', 'wc-price-format', 'wp-i18n', 'wp-plugins'), 'version' => '0d832adaf5a2c6233079');
+<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-blocks-components', 'wc-price-format', 'wp-i18n', 'wp-plugins'), 'version' => 'd6e7b1ac77fff0d4ef64');
