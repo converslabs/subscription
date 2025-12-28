735afeb8ff9bc472331c3128750738f19d7ced77
aushamim <au.shamim0@gmail.com>
2025-12-28 17:25:51 +0600

    refactor: 🛠️ generated a soft build file

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-blocks-components', 'wc-price-format', 'wp-i18n', 'wp-plugins'), 'version' => '7c48317a713a59d7e3b0');
+<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-blocks-components', 'wc-price-format', 'wp-i18n', 'wp-plugins'), 'version' => '8c6fd5bc67240ba195d4');
