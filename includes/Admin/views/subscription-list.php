c28eedfdd264a2bd7c1714c45cbcb3365abea572
aushamim <au.shamim0@gmail.com>
2026-05-18 16:39:54 +0600

    style: 🎨 input tag

--- includes/Admin/views/subscription-list.php
+++ includes/Admin/views/subscription-list.php
@@ -30,8 +30,10 @@ for ( $i = 0; $i < 12; $i++ ) {
 		<div class="wpsubs-toolbar">
 
 			<div class="wpsubs-search">
-				<svg class="wpsubs-search__icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-4.35-4.35M17 11A6 6 0 1 1 5 11a6 6 0 0 1 12 0z"/></svg>
-				<input type="search" name="s" value="<?php echo esc_attr( $search ); ?>" placeholder="<?php esc_attr_e( 'Search subscriptions...', 'subscription' ); ?>" class="wpsubs-search__input" />
+				<div class="wpsubs-input-wrap wpsubs-input-wrap--icon-l">
+					<svg class="wpsubs-input-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-4.35-4.35M17 11A6 6 0 1 1 5 11a6 6 0 0 1 12 0z"/></svg>
+					<input type="search" name="s" value="<?php echo esc_attr( $search ); ?>" placeholder="<?php esc_attr_e( 'Search subscriptions...', 'subscription' ); ?>" class="wpsubs-input" />
+				</div>
 			</div>
 
 			<?php
