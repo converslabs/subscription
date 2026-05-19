bfe67ed74888e756a2f7f4d7d5fc57dfe499bd6b
aushamim <au.shamim0@gmail.com>
2026-05-19 11:06:20 +0600

    fix: 🐛 same subscription quantity

--- templates/myaccount/subscriptions.php
+++ templates/myaccount/subscriptions.php
@@ -56,7 +56,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 				$trial_mode = get_post_meta( get_the_ID(), '_subscrpt_trial_mode', true );
 				$trial_mode = empty( $trial_mode ) ? 'off' : $trial_mode;
 
-				$price          = $subscription_data['price'] ?? 0;
+				$quantity       = (int) $order_item->get_quantity();
+				$price          = (float) ( $subscription_data['price'] ?? 0 ) * max( 1, $quantity );
 				$price_excl_tax = (float) $order_item->get_total();
 				$tax_amount     = (float) $order_item->get_total_tax();
 
