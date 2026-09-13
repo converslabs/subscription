13335b1c9295c8b9f68b738dbb7ae6197790b412
aushamim <au.shamim0@gmail.com>
2026-09-13 15:35:08 +0600

    fix: 🐛 integrations assets

--- includes/Admin/Integrations.php
+++ includes/Admin/Integrations.php
@@ -390,7 +390,7 @@ class Integrations {
 			'tutor_lms'   => [
 				'title'        => 'Tutor LMS',
 				'description'  => 'Restrict course access based on subscription status. Enroll and unenroll students automatically.',
-				'icon_url'     => SUBSCRPT_ASSETS . '/images/integrations/tutor-lms.gif',
+				'icon_url'     => SUBSCRPT_ASSETS . '/images/integrations/tutor-lms.jpeg',
 				'type'         => 'third_party',
 				'is_pro'       => true,
 				'category'     => 'lms',
@@ -413,7 +413,7 @@ class Integrations {
 			'learnpress'  => [
 				'title'        => 'LearnPress',
 				'description'  => 'Connect subscriptions with LearnPress courses. Enroll users automatically when subscriptions are active.',
-				'icon_url'     => SUBSCRPT_ASSETS . '/images/integrations/learnpress.gif',
+				'icon_url'     => SUBSCRPT_ASSETS . '/images/integrations/learnpress.png',
 				'type'         => 'third_party',
 				'is_pro'       => true,
 				'category'     => 'lms',
@@ -578,7 +578,7 @@ class Integrations {
 			'license_mgr' => [
 				'title'        => 'License Manager for WooCommerce',
 				'description'  => 'Generate and manage software license keys that are automatically tied to active subscriptions.',
-				'icon_url'     => SUBSCRPT_ASSETS . '/images/integrations/license-manager.gif',
+				'icon_url'     => SUBSCRPT_ASSETS . '/images/integrations/license-manager.png',
 				'type'         => 'third_party',
 				'is_pro'       => true,
 				'category'     => 'license',
